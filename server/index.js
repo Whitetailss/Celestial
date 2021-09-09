a3d9e11b8e04d6bfb338311496787eb109202bcd
sebastianGtomato <75072691+sebastianGtomato@users.noreply.github.com>
2021-09-09 18:15:49 +0800

    server changes

--- server/index.js
+++ server/index.js
@@ -14,11 +14,35 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://reactmaster123:<password>@cluster0.sxsba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+//SAMPLE APP MONGOOSE CONFIG
 
-mongoose.set('useFindAndModify', false);
+// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+// const PORT = process.env.PORT|| 5000;
+
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
+
+
+// MY CHANGES
+
+const CONNECTION_URL = 'mongodb+srv://reactmaster123:react123123@cluster0.sxsba.mongodb.net/reactmaster123?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+console.log(5000)
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) 
+.catch((error) => console.log(`${error} did not connect.`))
+
