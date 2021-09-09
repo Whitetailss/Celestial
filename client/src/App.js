d93b7f0cc338dfc432681cb8e90830ddf2501750
sebastianGtomato <75072691+sebastianGtomato@users.noreply.github.com>
2021-09-09 18:08:53 +0800

    form added to index

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import Typography from '@material-ui/core/Typography'
+import Form from './components/Form/Form';
 
 import useStyles from './styles';
+import { Grid, Container } from '@material-ui/core';
 
 function App() {
 
@@ -9,7 +11,11 @@ function App() {
   return (
     <div className="App">
       <Typography className={classes.heading} variant="h2" color="initial" align="center"> Celestial </Typography>
-
+        <Container >
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Container>
       </div>
       
   );
